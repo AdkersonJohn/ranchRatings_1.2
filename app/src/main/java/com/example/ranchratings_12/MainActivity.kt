3c9f8ebb2c3149509f7f24c925ac79e8ac3a6ebf
AdkersonJohn <61293447+AdkersonJohn@users.noreply.github.com>
2021-03-25 13:08:46 -0400

    initial files

---
+++ app/src/main/java/com/example/ranchratings_12/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.ranchratings_12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ranchratings_12.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
