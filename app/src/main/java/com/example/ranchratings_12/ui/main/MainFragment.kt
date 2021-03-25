65fd0768950738b842fbac28bfde73f4bf723dbe
AdkersonJohn <61293447+AdkersonJohn@users.noreply.github.com>
2021-03-25 19:06:04 -0400

    implemented permissions and necessary methods to access camera and store image bitmap to image view within main_fragment01.xml

--- app/src/main/java/com/example/ranchratings_12/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/ranchratings_12/ui/main/MainFragment.kt
@@ -1,18 +1,26 @@
 package com.example.ranchratings_12.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import android.Manifest
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ImageButton
-import androidx.lifecycle.Observer
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.ranchratings_12.R
 import kotlinx.android.synthetic.main.main_fragment01.*
 
+
 class MainFragment : Fragment() {
+
+    private val CAMERA_REQUEST_CODE = 1998
+    val CAMERA_PERMISSION_REQUEST_CODE = 1997
 
     companion object {
         fun newInstance() = MainFragment()
@@ -33,6 +41,57 @@ class MainFragment : Fragment() {
       /*  viewModel.institutions.observe(this, Observer{
             institutions -> actInstitutionName.setAdapter(ArrayAdapter(context!!, R.layout.support_simple_spinner_dropdown_item, institutions))
         })*/
+        btnTakePhoto.setOnClickListener {
+            prepTakePhoto()
+        }
+    }
+
+    private fun prepTakePhoto() {
+        if (ContextCompat.checkSelfPermission(context!!, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+            takePhoto()
+        }else{
+            val permissionRequest = arrayOf(Manifest.permission.CAMERA);
+            requestPermissions(permissionRequest, CAMERA_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            CAMERA_PERMISSION_REQUEST_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //permission granted lets do stuff
+                    takePhoto()
+                }else{
+                    Toast.makeText(context, "Unable to take photo without permission", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+    }
+    private fun takePhoto() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also{
+            takePictureIntent -> takePictureIntent.resolveActivity(context!!.packageManager)?.also {
+            startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE)
+        }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == RESULT_OK){
+            if(requestCode == CAMERA_REQUEST_CODE){
+                //now we can get the thumbnail
+                val imageBitmap = data!!.extras!!.get("data") as Bitmap
+                imgFood.setImageBitmap(imageBitmap)
+            }
+        }
     }
 
 }
