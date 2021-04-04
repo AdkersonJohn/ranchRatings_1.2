f89803e46ab234a68f8eadfd041daeb06022d5be
Mahesh Gowda <msg20017@gmail.com>
2021-04-04 18:25:16 -0400

    Revert "Eliminating redundancy and covering more use cases by using class ids instead of having the same params in multiple classes" Can't assume data structure and so changing DTOs is not reccomended This reverts commit 96c9cd33

--- app/src/main/java/com/example/ranchratings_12/dtos/Institution.kt
+++ app/src/main/java/com/example/ranchratings_12/dtos/Institution.kt
@@ -3,5 +3,5 @@ package com.example.ranchratings_12.dtos
 /**
  * Stores details on the restaurant or other institution reviewed.
  */
-class Institution (var institutionID: Int = 0, val name: String, val streetAddress: String, val phoneNumber: String, var locationID: Int = 0) {
+class Institution (val name: String, val streetAddress: String, val phoneNumber: String) {
 }
