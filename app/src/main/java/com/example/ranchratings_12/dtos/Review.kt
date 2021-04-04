96c9cd33cc65d169a78cea96d4ced9659ef908d3
Mahesh Gowda <msg20017@gmail.com>
2021-04-03 22:16:45 -0400

    Eliminating redundancy and covering more use cases by using class ids instead of having the same params in multiple classes

--- app/src/main/java/com/example/ranchratings_12/dtos/Review.kt
+++ app/src/main/java/com/example/ranchratings_12/dtos/Review.kt
@@ -3,5 +3,5 @@ package com.example.ranchratings_12.dtos
 /**
  * Stores the review itself, detailing the user, location, institution and rating.
  */
-data class Review (var userID: Int = 0, var institutionName: String = "", var latitude: String = "", var longitutde: String = "", var reviewText: String = "", var rating: Double = 0.0){
+data class Review (var reviewID: Int = 0, var userID: Int = 0, var institutionID: Int = 0, var reviewText: String = "", var rating: Double = 0.0){
 }
